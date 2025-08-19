205cd74afb726cd91754f82ed7fe0dde5c80774b
BoMin <lbmleebm@gmail.com>
2025-08-19 10:47:31 +0900

    Chore: Xcode 프로젝트 생성

---
+++ Afterschool/AfterschoolTests/AfterschoolTests.swift
@@ -0,0 +1,17 @@
+//
+//  AfterschoolTests.swift
+//  AfterschoolTests
+//
+//  Created by BoMin Lee on 8/19/25.
+//
+
+import Testing
+@testable import Afterschool
+
+struct AfterschoolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
