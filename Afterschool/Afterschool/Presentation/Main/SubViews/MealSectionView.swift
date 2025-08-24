d5292faa676f26fbc13b4c64c3d229dee533a972
BoMin <lbmleebm@gmail.com>
2025-08-25 00:40:19 +0900

    Feat: Meal Section View
    - Menu Carousel View 추후 추가 예정

---
+++ Afterschool/Afterschool/Presentation/Main/SubViews/MealSectionView.swift
@@ -0,0 +1,32 @@
+//
+//  MealSectionView.swift
+//  Afterschool
+//
+//  Created by BoMin Lee on 8/24/25.
+//
+
+import SwiftUI
+
+struct MealSectionView: View {
+    @ObservedObject var viewModel: MainViewModel
+    
+    init(viewModel: MainViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        VStack(spacing: 24) {
+            VStack(alignment: .leading, spacing: 8) {
+                Text("식단표")
+                    .font(.afBold20)
+                    .lineHeight(1.2, fontSize: 20)
+                    .foregroundStyle(Color.afGray900)
+                Text("학교 식단과 겹치지 않는 메뉴로 추천해줍니다.")
+                    .font(.afMedium14)
+                    .lineHeight(1.2, fontSize: 14)
+                    .foregroundStyle(Color.afGray600)
+            }
+            // TODO: MenuCarouseView
+        }
+    }
+}
