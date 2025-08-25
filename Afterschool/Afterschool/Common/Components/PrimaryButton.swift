5dc0d7d0e3f5467a7fd6c0a9c70198024570905d
임영택 <0tak2.code@gmail.com>
2025-08-25 17:39:09 +0900

    Feat: 공통 버튼 컴포넌트 (#15)
    * Feat: 공통 버튼
    
    * Feat: 프레임을 외부에서 지정하도록 변경
    
    * Feat: LocalizedStringKey 적용, 버튼 Enum으로 정의

---
+++ Afterschool/Afterschool/Common/Components/PrimaryButton.swift
@@ -0,0 +1,120 @@
+//
+//  PrimaryButton.swift
+//  Afterschool
+//
+//  Created by 임영택 on 8/25/25.
+//
+
+import SwiftUI
+
+/// 공통 컴포넌트 버튼
+struct PrimaryButton: View {
+    // MARK: - Properties
+    private let stringKey: LocalizedStringKey
+    private let disabled: Bool
+    private let didButtonTapped: () -> Void
+    
+    init(
+        _ stringKey: LocalizedStringKey,
+        disabled: Bool = false,
+        didButtonTapped: @escaping () -> Void
+    ) {
+        self.stringKey = stringKey
+        self.disabled = disabled
+        self.didButtonTapped = didButtonTapped
+    }
+    
+    init(
+        type: DefaultPrimaryButtonType,
+        disabled: Bool = false,
+        didButtonTapped: @escaping () -> Void
+    ) {
+        self.stringKey = type.label
+        self.disabled = disabled
+        self.didButtonTapped = didButtonTapped
+    }
+    
+    var body: some View {
+        Button(action: didButtonTapped) {
+            Text(stringKey)
+                .font(.afSemiBold18)
+                .foregroundStyle(Color.white)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+        .disabled(disabled)
+        .buttonStyle(PrimaryButtonStyle())
+    }
+}
+
+// MARK: - Button Type
+enum DefaultPrimaryButtonType {
+    case findSchool // 우리학교 등록하기
+    case getSuggestion // 추천받기
+    case retry // 다시뽑기
+    
+    var label: LocalizedStringKey {
+        switch self {
+        case .findSchool:
+            return "우리학교 등록하기"
+        case .getSuggestion:
+            return "추천받기"
+        case .retry:
+            return "다시뽑기"
+        }
+    }
+}
+
+// MARK: - ButtonStyle
+/// 공통 컴포넌트 버튼의 스타일. 눌렸을 때 버튼 상태를 정의한다.
+fileprivate struct PrimaryButtonStyle: ButtonStyle {
+    @Environment(\.isEnabled) private var isEnabled
+
+    func makeBody(configuration: Configuration) -> some View {
+        let baseColor: Color = isEnabled ? .afPrimary : .afGray100
+        let pressedColor: Color = isEnabled ? .afPrimaryDarken : .afGray100
+        let fill = configuration.isPressed ? pressedColor : baseColor
+
+        return configuration.label
+            .background(
+                RoundedRectangle(cornerRadius: 12, style: .circular)
+                    .fill(fill)
+            )
+    }
+}
+
+// MARK: - Frame 기본값
+struct PrimaryButtonDefaultFrame: ViewModifier {
+    private let defaultWidth: CGFloat = 393
+    private let defaultHeight: CGFloat = 64
+    
+    func body(content: Content) -> some View {
+        content
+            .frame(width: defaultWidth, height: defaultHeight)
+    }
+}
+
+extension View {
+    func primaryButtonDefaultFrame() -> some View {
+        modifier(PrimaryButtonDefaultFrame())
+    }
+}
+
+// MARK: - 프리뷰
+#Preview {
+    VStack {
+        PrimaryButton("테스트") {
+            print("버튼이 눌렸습니다.")
+        }
+        .primaryButtonDefaultFrame()
+        
+        PrimaryButton("테스트", disabled: true) {
+            print("버튼이 눌렸습니다.")
+        }
+        .primaryButtonDefaultFrame()
+        
+        PrimaryButton(type: .findSchool) {
+            print("버튼이 눌렸습니다.")
+        }
+        .primaryButtonDefaultFrame()
+    }
+}
