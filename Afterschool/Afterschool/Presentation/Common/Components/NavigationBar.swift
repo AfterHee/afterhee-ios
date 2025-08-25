46154ca2ceacdd4610d5813a60b14a2ea7f6ff1b
0tak2 <0tak2.code@gmail.com>
2025-08-25 15:15:50 +0900

    Feat: 공통 네비게이션 바

---
+++ Afterschool/Afterschool/Presentation/Common/Components/NavigationBar.swift
@@ -0,0 +1,97 @@
+//
+//  NavigationBar.swift
+//  Afterschool
+//
+//  Created by 임영택 on 8/25/25.
+//
+
+import SwiftUI
+
+/// 주어진 뷰를 VStack으로 감싸고, 상단에 커스텀 네비게이션 바를 붙이는 뷰 모디파이어
+///
+/// - Parameters:
+///   - centerView: 네비게이션 바의 중앙에 표시할 커스텀 뷰 (예: 텍스트, 로고 등)
+///   - inversed: 네비게이션 바 색상 반전 여부. `true`면 어두운 배경/흰색 아이콘, `false`면 밝은 배경/검정 아이콘
+struct NavigationBar<CenterView>: ViewModifier where CenterView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    /// MARK: - Properties
+    let centerView: CenterView
+    let inversed: Bool
+    
+    /// MARK: - Constraints
+    private let backButtonImageWidth: CGFloat = 15
+    private let backButtonImageHeight: CGFloat = 21
+    private let navBarHeight: CGFloat = 76
+    private let horizontalPadding: CGFloat = 20
+    private let inversedBackgroundColor: Color = Color(hex: "545454")
+    
+    func body(content: Content) -> some View {
+        VStack(spacing: 0) {
+            ZStack {
+                HStack { // 좌측 버튼
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "chevron.left")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .foregroundColor(!inversed ? Color.black : Color.white)
+                            .frame(width: backButtonImageWidth, height: backButtonImageHeight)
+                    }
+                    
+                    Spacer()
+                }
+                .frame(height: navBarHeight)
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal, horizontalPadding)
+                
+                HStack { // 가운데 뷰
+                    Spacer()
+                    centerView
+                        .frame(maxHeight: navBarHeight)
+                    Spacer()
+                }
+            }
+            .background(!inversed ? Color.white : inversedBackgroundColor)
+            .ignoresSafeArea(.all, edges: .horizontal)
+            .ignoresSafeArea(.all, edges: .bottom)
+            content
+            Spacer()
+        }
+    }
+}
+
+extension View {
+    func afNavigationBar<CenterView>(centerView: CenterView, inversed: Bool = false) -> some View where CenterView: View {
+        return modifier(NavigationBar(centerView: centerView, inversed: inversed))
+    }
+    
+    func afNavigationBar(title: String, inversed: Bool = false) -> some View {
+        let titleLabel = Text(title)
+            .font(.afSemiBold17)
+            .foregroundStyle(Color.black)
+        
+        return modifier(NavigationBar(
+            centerView: titleLabel,
+            inversed: inversed
+        ))
+    }
+}
+
+#Preview {
+    let demoCenterView = RoundedRectangle(cornerRadius: 8, style: .circular)
+        .foregroundStyle(Color.afGray50)
+        .padding(.leading, 51)
+        .padding(.trailing, 16)
+        .frame(height: 44)
+    
+    
+    VStack {
+        Spacer()
+        Text("contentView")
+        Spacer()
+    }
+        .afNavigationBar(centerView: demoCenterView)
+//        .afNavigationBar(title: "학교 변경하기")
+}
