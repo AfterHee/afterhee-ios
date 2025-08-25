acb5c00d75b2125f23bc2723055fce0671dee799
BoMin <lbmleebm@gmail.com>
2025-08-25 15:50:57 +0900

    WIP: MenuCarouselView

---
+++ Afterschool/Afterschool/Presentation/Main/SubViews/MenuCarouselView.swift
@@ -0,0 +1,14 @@
+//
+//  MenuCarouselView.swift
+//  Afterschool
+//
+//  Created by BoMin Lee on 8/25/25.
+//
+
+import SwiftUI
+
+struct MenuCarouselView: View {
+    var body: some View {
+        
+    }
+}
