578ffc30a82703e0a0a98424bf3269cc8dc75931
0tak2 <0tak2.code@gmail.com>
2025-08-26 15:42:29 +0900

    Feat: 로딩 뷰 타이틀

---
+++ Afterschool/Afterschool/Presentation/Result/SubViews/LoadingAndResultTitleLabel.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingAndResultTitleLabel.swift
+//  Afterschool
+//
+//  Created by 임영택 on 8/26/25.
+//
+
+
+import SwiftUI
+
+struct LoadingAndResultTitleLabel: View {
+    var body: some View {
+        Text("오늘 뭐 먹을까?")
+            .foregroundStyle(Color.afWhite)
+            .font(.afBold20)
+            .lineHeight(24 / 20, fontSize: 20)
+    }
+}
