37a5b8e6e073c3740f38a1d549db97203e92b828
0tak2 <0tak2.code@gmail.com>
2025-08-29 00:58:28 +0900

    Feat: LoadingAndResult 의존성 정의

--- Afterschool/Afterschool/Application/DIContainer/DIContainer.swift
+++ Afterschool/Afterschool/Application/DIContainer/DIContainer.swift
@@ -10,4 +10,5 @@ import Foundation
 final class DIContainer {
     lazy var navigationRouter: NavigationRouter = NavigationRouter()
     lazy var mainDepsProvider: MainDepsProviding = MainDepsProvider(navigationRouter: navigationRouter)
+    lazy var loadingAndResultDepsProvider: LoadingAndResultDepsProviding = LoadingAndResultDepsProvider(navigationRouter: navigationRouter)
 }
