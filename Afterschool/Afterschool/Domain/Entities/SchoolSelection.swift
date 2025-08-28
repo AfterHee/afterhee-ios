027c05dfabd82b6798319a00745a8944225fc5a2
BoMin <lbmleebm@gmail.com>
2025-08-29 01:21:38 +0900

    Feat: SchoolSelection Domain entity 추가

---
+++ Afterschool/Afterschool/Domain/Entities/SchoolSelection.swift
@@ -0,0 +1,14 @@
+//
+//  SchoolSelection.swift
+//  Afterschool
+//
+//  Created by BoMin Lee on 8/28/25.
+//
+
+import Foundation
+
+struct SchoolSelection: Codable, Equatable {
+    let name: String
+    let eduOfficeCode: String
+    let adminCode: String
+}
