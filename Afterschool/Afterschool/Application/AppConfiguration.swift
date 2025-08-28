7b3f70297e9c3d775608c40583e31e7d4877f1c9
BoMin <lbmleebm@gmail.com>
2025-08-28 21:23:52 +0900

    Feat: AppConfiguration

--- Afterschool/Afterschool/Application/AppConfiguration.swift
+++ Afterschool/Afterschool/Application/AppConfiguration.swift
@@ -5,3 +5,18 @@
 //  Created by BoMin Lee on 8/24/25.
 //
 
+import Foundation
+
+enum AppConfigurationError: Error {
+    case missingOrInvalidBaseURL
+}
+
+struct AppConfiguration {
+    static var baseURL: URL {
+        guard let s = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as? String,
+              let url = URL(string: s) else {
+            preconditionFailure("BASE_URL is missing or invalid in Info.plist")
+        }
+        return url
+    }
+}
