d53605f11672d8fbaa6d621505cf687f97e6fad6
BoMin <lbmleebm@gmail.com>
2025-08-28 15:26:39 +0900

    Fix: Onboarding 여부에 따른 MainView 분기

--- Afterschool/Afterschool/Presentation/Main/MainView.swift
+++ Afterschool/Afterschool/Presentation/Main/MainView.swift
@@ -11,28 +11,37 @@ struct MainView: View {
     @StateObject private var viewModel = MainViewModel()
     
     var body: some View {
-        ZStack {
-            ScrollView {
-                VStack(spacing: 56) {
-                    VStack(alignment: .leading, spacing: 16) {
-                        SchoolHeaderView(viewModel: viewModel)
-                        MealSectionView(viewModel: viewModel)
+        NavigationStack {
+            if viewModel.shouldShowOnboarding {
+                OnboardingView(shouldShowOnboarding: $viewModel.shouldShowOnboarding)
+            } else {
+                ZStack {
+                    ScrollView {
+                        VStack(spacing: 56) {
+                            VStack(alignment: .leading, spacing: 16) {
+                                SchoolHeaderView(viewModel: viewModel)
+                                MealSectionView(viewModel: viewModel)
+                            }
+                            CategorySelectSectionView(viewModel: viewModel)
+                            
+                        }
+                        .safeAreaPadding(.horizontal, 16)
+                        Spacer().frame(height: 120)
                     }
-                    CategorySelectSectionView(viewModel: viewModel)
-                    
+                    VStack {
+                        Spacer()
+                        
+                        PrimaryButton(type: .getSuggestion, disabled: viewModel.selectedCategory == nil) {
+                            viewModel.getRecommendationButtonTapped()
+                        }
+                        .primaryButtonDefaultFrame()
+                        .safeAreaPadding(.horizontal, 16)
+                    }
                 }
-                .safeAreaPadding(.horizontal, 16)
-                Spacer().frame(height: 120)
             }
-            VStack {
-                Spacer()
-                
-                PrimaryButton(type: .getSuggestion, disabled: viewModel.selectedCategory == nil) {
-                    viewModel.getRecommendationButtonTapped()
-                }
-                .primaryButtonDefaultFrame()
-                .safeAreaPadding(.horizontal, 16)
-            }
+        }
+        .onAppear {
+            viewModel.shouldShowOnboarding = !UserDefaults.standard.bool(forKey: UserDefaultsKey.onboardingShown.rawValue)
         }
     }
 }
