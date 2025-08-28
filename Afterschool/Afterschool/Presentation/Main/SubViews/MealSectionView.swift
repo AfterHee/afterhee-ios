4d5344c9a2daae5eb458980c7b03465c047740d1
BoMin <lbmleebm@gmail.com>
2025-08-28 12:21:33 +0900

    Fix: MealSectionView
    - CarouselView 추가

--- Afterschool/Afterschool/Presentation/Main/SubViews/MealSectionView.swift
+++ Afterschool/Afterschool/Presentation/Main/SubViews/MealSectionView.swift
@@ -16,17 +16,23 @@ struct MealSectionView: View {
     
     var body: some View {
         VStack(spacing: 24) {
-            VStack(alignment: .leading, spacing: 8) {
-                Text("식단표")
-                    .font(.afBold20)
-                    .lineHeight(1.2, fontSize: 20)
-                    .foregroundStyle(Color.afGray900)
-                Text("학교 식단과 겹치지 않는 메뉴로 추천해줍니다.")
-                    .font(.afMedium14)
-                    .lineHeight(1.2, fontSize: 14)
-                    .foregroundStyle(Color.afGray600)
+            HStack {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("식단표")
+                        .font(.afBold20)
+                        .lineHeight(1.2, fontSize: 20)
+                        .foregroundStyle(Color.afGray900)
+                    Text("학교 식단과 겹치지 않는 메뉴로 추천해줍니다.")
+                        .font(.afMedium14)
+                        .lineHeight(1.2, fontSize: 14)
+                        .foregroundStyle(Color.afGray600)
+                }
+                Spacer()
             }
-            // TODO: MenuCarouseView
+            MenuCarouselView(
+                menus: viewModel.menus,
+                selectedIndex: $viewModel.selectedMenuIndex
+            )
         }
     }
 }
