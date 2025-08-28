da580eafdf69b8449f6ea1b3f3fa7ea5b0165b08
0tak2 <0tak2.code@gmail.com>
2025-08-29 03:33:55 +0900

    Feat: 학교 검색 의존성 정의

---
+++ Afterschool/Afterschool/Application/DIContainer/SchoolSearchDepsProvider.swift
@@ -0,0 +1,31 @@
+//
+//  SchoolSearchDepsProvider.swift
+//  Afterschool
+//
+//  Created by 임영택 on 8/29/25.
+//
+
+import Foundation
+
+final class SchoolSearchDepsProvider: SchoolSearchDepsProviding {
+    let navigationRouter: NavigationRouter
+    
+    init(navigationRouter: NavigationRouter) {
+        self.navigationRouter = navigationRouter
+    }
+    
+    func getSchoolSearchViewModel() -> SchoolSearchViewModel {
+        let mainServerRepository = MainServerRepository()
+        let getSchoolUseCase = GetSchoolUseCase(repository: mainServerRepository)
+        
+        let userDefaultsStorage = UserDefaultsStorage()
+        let userDefaultsRepository = UserDefaultsRepository(storage: userDefaultsStorage)
+        let setSelectedSchoolUseCase = SetSelectedSchoolUseCase(userDefaultRepository: userDefaultsRepository)
+        
+        return SchoolSearchViewModel(
+            navigationRouter: navigationRouter,
+            getSchoolUseCase: getSchoolUseCase,
+            setSelectedSchoolUseCase: setSelectedSchoolUseCase
+        )
+    }
+}
