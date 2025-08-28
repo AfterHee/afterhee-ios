285fcc77ac69fccb2d2180ec4164a6c79d8e9791
BoMin <lbmleebm@gmail.com>
2025-08-28 21:24:35 +0900

    Feat: SuggestDTO

---
+++ Afterschool/Afterschool/Infrastructure/Network/DTO/SuggestDTO.swift
@@ -0,0 +1,13 @@
+//
+//  SuggestDTO.swift
+//  Afterschool
+//
+//  Created by BoMin Lee on 8/28/25.
+//
+
+import Foundation
+
+struct SuggestDTO: Codable {
+    let recommendation: String
+    let reason: String
+}
