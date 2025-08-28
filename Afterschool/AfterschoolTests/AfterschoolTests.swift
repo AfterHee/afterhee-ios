e2c1808633670bea495234fde1cb40746c56d82b
BoMin <lbmleebm@gmail.com>
2025-08-28 21:25:09 +0900

    Feat: getSchool 테스트

--- Afterschool/AfterschoolTests/AfterschoolTests.swift
+++ Afterschool/AfterschoolTests/AfterschoolTests.swift
@@ -9,9 +9,15 @@ import Testing
 @testable import Afterschool
 
 struct AfterschoolTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test func testGetSchool() async throws {
+        let network = Network()
+        let target: MainServerTarget = .getSchool(keyword: "포항제철")
+        
+        let response: BaseResponseDTO<[SchoolDTO]> = try await network.task(target)
+        
+        #expect(response.isError == false)
+        #expect(!response.data.isEmpty)
+        print("받은 학교 개수:", response.data.count)
+        print("첫 번째 학교:", response.data.first?.schoolName ?? "nil")
     }
-
 }
