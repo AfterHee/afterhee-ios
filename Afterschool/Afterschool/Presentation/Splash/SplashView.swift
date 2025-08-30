c6e51162c2ab2a7b414be79089fa8277f358d7ce
BoMin <lbmleebm@gmail.com>
2025-08-30 16:27:18 +0900

    Feat: SplashView

---
+++ Afterschool/Afterschool/Presentation/Splash/SplashView.swift
@@ -0,0 +1,17 @@
+//
+//  SplashView.swift
+//  Afterschool
+//
+//  Created by BoMin Lee on 8/30/25.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        ZStack(alignment: .center) {
+            Color.afWhite
+            Image(.appTextLogo)
+        }
+    }
+}
